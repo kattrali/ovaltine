b27b3cb9129e1f66772a38d350c93c6b1fd88879
Delisa Mason <iskanamagus@gmail.com>
2014-08-02 18:36:42 -0700

    Hotfix line breaks on Ruby 1.8.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.2
+
+- Hotfix for Ruby 1.8.7
+
 # 1.0.1
 
 - Make error message more verbose
--- lib/ovaltine/objc/storyboard_formatter.rb
+++ lib/ovaltine/objc/storyboard_formatter.rb
@@ -32,30 +32,30 @@ module Ovaltine
     private
 
     def generate_header
-      StoryboardTemplates::HEADER_TEMPLATE
-        .gsub('{FILENAME}', "#{classname}.h")
-        .gsub('{CLASS_NAME}', classname)
-        .gsub('{REUSE_IDENTIFIERS}', reuse_identifier_definitions)
-        .gsub('{SEGUE_IDENTIFIERS}', segue_identifier_definitions)
+      StoryboardTemplates::HEADER_TEMPLATE\
+        .gsub('{FILENAME}', "#{classname}.h")\
+        .gsub('{CLASS_NAME}', classname)\
+        .gsub('{REUSE_IDENTIFIERS}', reuse_identifier_definitions)\
+        .gsub('{SEGUE_IDENTIFIERS}', segue_identifier_definitions)\
         .gsub('{VIEW_CONTROLLERS}', view_controller_definitions)
     end
 
     def generate_implementation
-      StoryboardTemplates::IMPLEMENTATION_TEMPLATE
-        .gsub('{FILENAME}', "#{classname}.m")
-        .gsub('{CLASS_NAME}', classname)
-        .gsub('{STATIC_VARIABLES}', static_variables)
-        .gsub('{REUSE_IDENTIFIERS}', reuse_identifier_implementations)
-        .gsub('{SEGUE_IDENTIFIERS}', segue_identifier_implementations)
-        .gsub('{VIEW_CONTROLLERS}', view_controller_implementations)
+      StoryboardTemplates::IMPLEMENTATION_TEMPLATE\
+        .gsub('{FILENAME}', "#{classname}.m")\
+        .gsub('{CLASS_NAME}', classname)\
+        .gsub('{STATIC_VARIABLES}', static_variables)\
+        .gsub('{REUSE_IDENTIFIERS}', reuse_identifier_implementations)\
+        .gsub('{SEGUE_IDENTIFIERS}', segue_identifier_implementations)\
+        .gsub('{VIEW_CONTROLLERS}', view_controller_implementations)\
         .gsub('{STORYBOARD}', StoryboardTemplates::STORYBOARD_IMPLEMENTATION_TEMPLATE.gsub('{IDENTIFIER_CONSTANT_NAME}', variable_name(storyboard.name)))
     end
 
     def static_variables
       identifiers = (storyboard.cell_reuse_identifiers + storyboard.view_controller_identifiers + storyboard.segue_identifiers + [storyboard.name]).sort
       identifiers.map do |identifier|
-        StoryboardTemplates::STATIC_IDENTIFIER_TEMPLATE
-          .gsub('{IDENTIFIER}', identifier)
+        StoryboardTemplates::STATIC_IDENTIFIER_TEMPLATE\
+          .gsub('{IDENTIFIER}', identifier)\
           .gsub('{IDENTIFIER_CONSTANT_NAME}', variable_name(identifier))
       end.join("\n")
     end
@@ -68,8 +68,8 @@ module Ovaltine
 
     def reuse_identifier_implementations
       prepend_title(CELL_REUSE_SECTION_TITLE, storyboard.cell_reuse_identifiers.each_with_index.map do |identifier, index|
-        StoryboardTemplates::REUSE_IMPLEMENTATION_TEMPLATE
-          .gsub('{IDENTIFIER}', format_reuse_identifier(identifier))
+        StoryboardTemplates::REUSE_IMPLEMENTATION_TEMPLATE\
+          .gsub('{IDENTIFIER}', format_reuse_identifier(identifier))\
           .gsub('{IDENTIFIER_CONSTANT_NAME}', variable_name(identifier))
       end.join("\n"))
     end
@@ -82,8 +82,8 @@ module Ovaltine
 
     def segue_identifier_implementations
       prepend_title(SEGUE_SECTION_TITLE, storyboard.segue_identifiers.each_with_index.map do |identifier, index|
-        StoryboardTemplates::SEGUE_IMPLEMENTATION_TEMPLATE
-          .gsub('{IDENTIFIER}', format_segue_identifier(identifier))
+        StoryboardTemplates::SEGUE_IMPLEMENTATION_TEMPLATE\
+          .gsub('{IDENTIFIER}', format_segue_identifier(identifier))\
           .gsub('{IDENTIFIER_CONSTANT_NAME}', variable_name(identifier))
       end.join("\n"))
     end
@@ -96,8 +96,8 @@ module Ovaltine
 
     def view_controller_implementations
       prepend_title(VIEW_CONTROLLER_SECTION_TITLE, storyboard.view_controller_identifiers.each_with_index.map do |identifier, index|
-        StoryboardTemplates::VIEW_CONTROLLER_IMPLEMENTATION_TEMPLATE
-          .gsub('{IDENTIFIER}', variable_name(identifier))
+        StoryboardTemplates::VIEW_CONTROLLER_IMPLEMENTATION_TEMPLATE\
+          .gsub('{IDENTIFIER}', variable_name(identifier))\
           .gsub('{CAPITALIZED_IDENTIFIER}', format_view_controller(identifier))
       end.join("\n"))
     end
